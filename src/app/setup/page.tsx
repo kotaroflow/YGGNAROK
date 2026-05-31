fafc42a05764607fc126d016a71298bb403071d6
kotaroflow <naoteemteresa@gmail.com>
2026-05-31 06:23:09 -0300

    fix: force dynamic rendering on /setup route to fix cookies static error

--- src/app/setup/page.tsx
+++ src/app/setup/page.tsx
@@ -1,6 +1,8 @@
 import { ensureInitialWorkspace } from "@/server/setup/initial-workspace";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default async function SetupPage() {
   try {
     await ensureInitialWorkspace();
